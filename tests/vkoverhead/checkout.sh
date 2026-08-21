9ced5b236f936a43dec469d535852bace11db069
Romaric Jodin <rjodin@google.com>
2026-08-21 09:31:06 +0200

    update vkoverhead version to latest
    Change-Id: I5acab77e18ce858d5d7481cbc35f928c2d17d3b7

--- tests/vkoverhead/checkout.sh
+++ tests/vkoverhead/checkout.sh
@@ -9,4 +9,4 @@ DEST="$1"
 SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
 bash "${SCRIPT_DIR}/../checkout.sh" https://gitlab.freedesktop.org/mesa/mesa 742af792775f21e29e154a4e35b8c1a2e3103de4 "${DEST}/external/mesa3d"
-bash "${SCRIPT_DIR}/../checkout.sh" https://github.com/zmike/vkoverhead.git 9f3647dbcdb7a67814933e99834d2864c431c217 "${DEST}/external/vkoverhead"
+bash "${SCRIPT_DIR}/../checkout.sh" https://github.com/zmike/vkoverhead.git a1c3001c519425b05ea580b130ef096427ba26b6 "${DEST}/external/vkoverhead"
